a9084ea6a22d5d9803b6813bc258b61a22a6d4b6
QuentinEPSII <73177942+QuentinEPSII@users.noreply.github.com>
2021-05-19 15:35:30 +0200

    coucou

---
+++ script.sql
@@ -0,0 +1,100 @@
+USE mspr_java;
+
+DROP TABLE BusinessIncident;
+DROP TABLE Users_Worksite;
+DROP TABLE BusinessProject;
+DROP TABLE Worksite;
+DROP TABLE Projects;
+DROP TABLE Video;
+DROP TABLE Users;
+DROP TABLE Incident;
+DROP TABLE Business;
+
+
+CREATE TABLE Users (
+	id INT IDENTITY(1,1),
+	firstname nvarchar(255),
+	lastname nvarchar(255),
+    rights nvarchar(255),
+    yearStart nvarchar(255),   
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE Incident (
+	id INT IDENTITY(1,1),
+    idVideo INT,
+	description nvarchar(255),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE Video (
+	id INT NOT NULL,
+    idIncident INT,
+	name nvarchar(255),
+    refGant INT,
+    indoor bit,
+    latitude DECIMAL,
+    longitude DECIMAL,
+    video nvarchar(255),
+	PRIMARY KEY(id),
+    CONSTRAINT Video_Incident_FK FOREIGN KEY (idIncident) REFERENCES Incident(id)
+);
+
+CREATE TABLE Projects (
+	id INT NOT NULL,
+    idVideo INT,
+	startDate date,
+    endDate date,
+	idChantier INT,
+	name nvarchar(255),
+	PRIMARY KEY(id),
+    CONSTRAINT Projects_Video_FK FOREIGN KEY (idVideo) REFERENCES Video(id)
+);
+
+CREATE TABLE Worksite (
+	id INT NOT NULL,
+    idProject INT,
+	latitude DECIMAL,
+    longitude DECIMAL,
+    name nvarchar(255),
+    startDate DATE,
+    endDate DATE,
+	PRIMARY KEY(id),
+    CONSTRAINT Worksite_Project_FK FOREIGN KEY (idProject) REFERENCES Projects(id)
+);
+
+CREATE TABLE Users_Worksite (
+	idChantier INT NOT NULL,
+	idUser INT NOT NULL,
+	PRIMARY KEY(idChantier, idUser),
+    CONSTRAINT UserWorksite_User_FK FOREIGN KEY (idUser) REFERENCES Users(id),
+    CONSTRAINT UserWorksite_Worksite_FK FOREIGN KEY (idChantier) REFERENCES Worksite(id)
+);
+
+CREATE TABLE Business (
+	siret INT NOT NULL,
+    name nvarchar(255),
+    address nvarchar(255),
+    employees INT,
+    phone nvarchar(16),
+    mail nvarchar(255),
+	description nvarchar(255),
+	PRIMARY KEY(siret)
+);
+
+CREATE TABLE BusinessIncident (
+	idIncident INT NOT NULL,
+    idBusiness INT NOT NULL,
+	PRIMARY KEY(idIncident, idBusiness),
+    CONSTRAINT BusinessIncident_Incident_FK FOREIGN KEY (idIncident) REFERENCES Incident(id),
+    CONSTRAINT BusinessIncident_Business_FK FOREIGN KEY (idBusiness) REFERENCES Business(siret)
+);
+
+CREATE TABLE BusinessProject (
+	idBusiness INT NOT NULL,
+    idProject INT NOT NULL,
+	PRIMARY KEY(idBusiness, idProject),
+    CONSTRAINT BusinessProject_Business_FK FOREIGN KEY (idBusiness) REFERENCES Business(siret),
+    CONSTRAINT BusinessProject_Project_FK FOREIGN KEY (idProject) REFERENCES Projects(id)
+);
+
