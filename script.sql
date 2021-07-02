0375926c1efbff3fff714e1c21e80b471af46ec7
Nicolas Ubuntu <ngaly@hotmail.fr>
2021-07-02 16:32:05 +0200

    add login field as PK in User table

--- script.sql
+++ script.sql
@@ -11,7 +11,7 @@ DROP TABLE Users;
 DROP TABLE Business;
 
 CREATE TABLE Users (
-	id INT NOT NULL IDENTITY(1,1),
+	login nvarchar(255) PRIMARY KEY,
 	firstname nvarchar(255),
 	lastname nvarchar(255),
     rights nvarchar(255),
@@ -105,11 +105,11 @@ CREATE TABLE BusinessProject (
 --insert into Worksite(latitude, longitude, name, startDate, endDate) values ('-33.867886', '-63.987', 'Guitare', '2012-21-10', '2012-21-12');
 
 
-insert into Users(firstname, lastname, rights, yearStart) values ('Frédéric', 'Pouchan', 'ADMIN', 2012);
-insert into Users(firstname, lastname, rights, yearStart) values ('Quentin', 'GEORGHIOU', 'ADMIN', 2012);
-insert into Users(firstname, lastname, rights, yearStart) values ('Nicolas', 'Galy', 'ADMIN', 2012);
-insert into Users(firstname, lastname, rights, yearStart) values ('David', 'Cerna', 'ADMIN', 2012);
-insert into Users(firstname, lastname, rights, yearStart) values ('Larbin', 'larbin', 'USER', 2010);
+insert into Users(login, firstname, lastname, rights, yearStart) values ('fpouchan', 'Frédéric', 'Pouchan', 'ADMIN', 2012);
+insert into Users(login, firstname, lastname, rights, yearStart) values ('qgeorghioui', 'Quentin', 'GEORGHIOU', 'ADMIN', 2012);
+insert into Users(login, firstname, lastname, rights, yearStart) values ('ngaly', 'Nicolas', 'Galy', 'ADMIN', 2012);
+insert into Users(login, firstname, lastname, rights, yearStart) values ('dcerna', 'David', 'Cerna', 'ADMIN', 2012);
+insert into Users(login, firstname, lastname, rights, yearStart) values ('llarbin', 'Larbin', 'larbin', 'USER', 2010);
 
 insert into Worksite(id, latitude, longitude, name, startDate, endDate) values (1, '45.186973698061685', '5.7770144356323145', 'EPSI GRENOBLE', '2013-07-05', '2014-11-14');
 insert into Worksite(id, latitude, longitude, name, startDate, endDate) values (2, '33.747252', '112.633853', 'Triangle', '2012-10-10', '2012-11-10');
