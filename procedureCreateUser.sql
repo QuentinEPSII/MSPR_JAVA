cf3cda1d92a4c63157a4f323d34400349a543130
Nicolas Galy <ngaly@hotmail.fr>
2021-07-06 16:18:57 +0200

    [ADDED] connectUser procedure, [UPDATED] createUser

--- procedureCreateUser.sql
+++ procedureCreateUser.sql
@@ -1,7 +1,7 @@
 CREATE OR ALTER PROCEDURE CreateUser 
 	@CodeApplication char(1),
-	@Nom nvarchar(50),
-	@Prenom nvarchar(50),
+	@Nom nvarchar(255),
+	@Prenom nvarchar(255),
 	@ChantierId int
 AS
 	SET NOCOUNT ON;
